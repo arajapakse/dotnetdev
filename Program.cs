42a0ae935b2a4b61b3ff4f23da22ccf30255aa53
Asanka <arajapakse@billtrust.com>
2018-02-26 11:08:24 -0500

    command line runnable version.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace PizzaLoversApi
+namespace PizzaLoversDemo2
 {
     public class Program
     {
--- Startup.cs
+++ Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace PizzaLoversApi
+namespace PizzaLoversDemo2
 {
     public class Startup
     {
